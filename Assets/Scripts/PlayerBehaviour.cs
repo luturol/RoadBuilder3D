4a4e4b6a7e9b9b659faa8d93ec24e2410ed9d0c6
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-06-24 11:15:57 -0300

    add resize script

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField] private Transform roadPrefab;
+
+    private Transform currentPlatform;
+    private bool firstClickSpace;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(firstClickSpace == false)
+            {
+                
+            }
+
+            Debug.Log("pressionando espaço");
+
+            if(roadPrefab != null)
+            {
+                Resize(roadPrefab, 1, new Vector3(0f, 0.01f, 0f));
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            currentPlatform = other.gameObject.transform;
+            Debug.Log("Estmoas na plataform", currentPlatform);
+        }
+    }
+
+    private void Resize(Transform currentPlatform, float amount, Vector3 direction)
+    {        
+        currentPlatform.position += direction * amount / 2;
         
+        currentPlatform.localScale += direction * amount;
     }
 }
